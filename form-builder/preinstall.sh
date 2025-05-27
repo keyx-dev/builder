60de1172d8db3b6a54396d3784fdbc82fb677d19
Cihad Paksoy <cihadp@gmail.com>
2025-05-27 03:13:45 +0300

    fix: update preinstall script to ensure pnpm-lock.yaml is generated

--- form-builder/preinstall.sh
+++ form-builder/preinstall.sh
@@ -16,11 +16,23 @@ EOL
   # package.json dosyasını oluştur, varsa üzerine yazma
   if [ ! -f ../package.json ]; then
     cat > ../package.json << EOL
-{}
+{
+  "packageManager": "pnpm@10.11.0"
+}
 EOL
     echo "package.json oluşturuldu."
   else
     echo "package.json zaten var, üzerine yazılmadı."
+  fi
+
+  # package.json dosyasını oluştur, varsa üzerine yazma
+  if [ ! -f ../pnpm-lock.yaml ]; then
+    cat > ../pnpm-lock.yaml << EOL
+
+EOL
+    echo "pnpm-lock.yaml oluşturuldu."
+  else
+    echo "pnpm-lock.yaml zaten var, üzerine yazılmadı."
   fi
 else
   echo "Netlify ortamı değil, preinstall script'i çalıştırılmadı."
